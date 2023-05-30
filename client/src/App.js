5ce9be2fc21c699c4740574254e2d1dab24418b1
joe-shajan <joeshajan1551@gmail.com>
2023-05-30 19:59:53 +0530

    client refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,75 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import "./App.css";
+import styles from "./styles.module.css";
+
+function Home(userDetails) {
+  const user = userDetails.user;
+  const logout = () => {
+    window.open(`${process.env.REACT_APP_API_URL}/auth/logout`, "_self");
+  };
+
+  return (
+    <div className={styles.right}>
+      <img src={user.picture} alt="profile" className={styles.profile_img} />
+      <input type="text" defaultValue={user.name} className={styles.input} />
+      <input type="text" defaultValue={user.email} className={styles.input} />
+      <button className={styles.btn} onClick={logout}>
+        Log Out
+      </button>
+    </div>
+  );
+}
+
+function Login() {
+  console.log(`${process.env.REACT_APP_API_URL}/auth/google/callback`);
+  const googleAuth = () => {
+    window.open(
+      `${process.env.REACT_APP_API_URL}/auth/google/callback`,
+      "_self"
+    );
+  };
+  return (
+    <button className={styles.google_btn} onClick={googleAuth}>
+      <img src="./images/google.png" alt="google icon" />
+      <span>Sign in with Google</span>
+    </button>
+  );
+}
 
 function App() {
-	const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-	const getUser = async () => {
-		try {
-			const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
-			const { data } = await axios.get(url, { withCredentials: true });
-			setUser(data.user._json);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const getUser = async () => {
+    try {
+      const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
+      const { data } = await axios.get(url, { withCredentials: true });
+      setUser(data.user._json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	useEffect(() => {
-		getUser();
-	}, []);
+  useEffect(() => {
+    getUser();
+  }, []);
 
-	return (
-		<div className="container">
-			<Routes>
-				<Route
-					exact
-					path="/"
-					element={user ? <Home user={user} /> : <Navigate to="/login" />}
-				/>
-				<Route
-					exact
-					path="/login"
-					element={user ? <Navigate to="/" /> : <Login />}
-				/>
-				<Route
-					path="/signup"
-					element={user ? <Navigate to="/" /> : <Signup />}
-				/>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="container">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home user={user} /> : <Navigate to="/login" />}
+        />
+        <Route
+          exact
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
